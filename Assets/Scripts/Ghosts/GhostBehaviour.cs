d871cd084da7f69a76b3c5e79464ba2f89640f8b
dhsong <77286910+dhsong215@users.noreply.github.com>
2024-05-09 21:56:25 +0900

    Ghost behaviour classes are inherit from GhostBehaviour class

--- Assets/Scripts/Ghosts/GhostBehaviour.cs
+++ Assets/Scripts/Ghosts/GhostBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ghosty : MonoBehaviour
+public class GhostBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
