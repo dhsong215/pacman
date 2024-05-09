3d9e3d259693137ed38e8dcb4a5dba4f5af20e87
dhsong <77286910+dhsong215@users.noreply.github.com>
2024-05-10 01:14:36 +0900

    Add basic behaviour (GhostBehaviour)

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    // 고스트의 점수
+    public int points = 200;
+}
---
+++ Assets/Scripts/GhostBehaviour.cs
@@ -0,0 +1,36 @@
+// 이 스크립트는 Ghost들의 모드를 관리합니다.
+using UnityEngine;
+
+[RequireComponent(typeof(Ghost))]
+public class GhostBehaviour : MonoBehaviour
+{
+    public Ghost Ghost { get; private set; }
+
+    public float duration;
+
+    // 게임시작후 오브젝트 생성 시 실행
+    private void Awake() {
+        this.Ghost = GetComponent<Ghost>();
+        this.enabled = false;
+    }
+
+    // 특정 행동 제어 타이머 
+    public void Enable() {
+        // 인자 없이 enable실행시 기본 duration값 사용
+        Enable(this.duration);
+    }
+
+    public virtual void Enable(float duration) {
+        this.enabled = true;
+
+        CancelInvoke(); // Invoke 끝나기 전에 다른 파워쿠키 먹으면 현재 Invoke 취소후 다시 Invoke
+        Invoke(nameof(Disable), duration); // duration 지난 후 Disable
+    }
+
+    public virtual void Disable() {
+        this.enabled = false;
+
+        CancelInvoke();
+    }
+
+}
