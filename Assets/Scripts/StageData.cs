14d42cfe3a18e2630f0975435160b402f7f31600
SeungHyeonOh <osh200420@gmail.com>
2024-05-10 14:06:50 +0900

    Stage Data Script

--- Assets/Scripts/StageData.cs
+++ Assets/Scripts/StageData.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageData : MonoBehaviour
+public class StageData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Vector2 limitMin;
+    [SerializeField]
+    private Vector2 limitMax;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Vector2 LimitMin => limitMin;
+    public Vector2 LimitMax => limitMax;
 }
+
+
+/* 
+ * File : StageData.cs
+ * Desc 
+ *  : 현재 스테이지의 화면 내 범위
+ *  : 에셋 데이터로 저장해두고 정보를 불러와서 사용
+ *
+ */
