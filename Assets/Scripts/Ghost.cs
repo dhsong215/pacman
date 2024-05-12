96f7c0a332ece7c55d50d09da4180d44e3dad2e1
dhsong <77286910+dhsong215@users.noreply.github.com>
2024-05-12 23:11:28 +0900

    캐릭터 초기 상태 정의 및 고스트 상태 리셋 추가

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class Ghost : MonoBehaviour
 {
     // home, scatter, chase, frightened간 서로 참조하며 전환 위해 프로퍼티 추가
-    public Movement movement {get; private set;}
-    public GhostHome home {get; private set;}
-    public GhostScatter scatter {get; private set;}
-    public GhostChase chase {get; private set;}
-    public GhostFrightened frightened {get; private set;}
+    public Movement Movement {get; private set;}
+    public GhostHome Home {get; private set;}
+    public GhostScatter Scatter {get; private set;}
+    public GhostChase Chase {get; private set;}
+    public GhostFrightened Frightened {get; private set;}
     public GhostBehaviour initialBehaviour;
     public Transform target;
 
@@ -19,11 +19,33 @@ public class Ghost : MonoBehaviour
 
     private void Awake()
     {
-        this.movement = GetComponent<Movement>();
-        this.home = GetComponent<GhostHome>();
-        this.scatter = GetComponent<GhostScatter>();
-        this.chase = GetComponent<GhostChase>();
-        this.frightened = GetComponent<GhostFrightened>();
+        this.Movement = GetComponent<Movement>();
+        this.Home = GetComponent<GhostHome>();
+        this.Scatter = GetComponent<GhostScatter>();
+        this.Chase = GetComponent<GhostChase>();
+        this.Frightened = GetComponent<GhostFrightened>();
+    }
+
+    public void Start()
+    {
+        ResetState();
+    }
+
+    public void ResetState()
+    {
+        this.gameObject.SetActive(true);
+        this.Movement.ResetState();
+        this.Scatter.Enable();
+        this.Frightened.Disable();
+        this.Chase.Disable();
+
+        if (this.Home != this.initialBehaviour) {
+            this.Home.Disable();
+        }
+
+        if (this.initialBehaviour != null) {
+            this.initialBehaviour.Enable();
+        }
     }
 
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
+// Rigidbody2D 컴포넌트를 필요로 하는 Movement 클래스를 정의합니다.
+// 여러 캐릭터의 움직임을 설정합니다.
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 8f; // 기본 이동 속도
+    public float speedMultiplier = 1f; // 속도 배수
+    public Vector2 initialDirection; // 초기 이동 방향
+    public LayerMask obstacleLayer; // 장애물 감지용 레이어 마스크
 
-    // Update is called once per frame
-    void Update()
+    public Rigidbody2D Rigidbody { get; private set; }
+    public Vector2 Direction { get; private set; } // 현재 이동 방향
+    public Vector2 NextDirection { get; private set; } // 다음 이동 방향
+    public Vector3 StartingPosition { get; private set; } // 시작 위치
+    
+    // 캐릭터 상태를 초기 상태로 리셋합니다.
+    public void ResetState()
     {
-        
+        speedMultiplier = 1f;
+        Direction = initialDirection;
+        NextDirection = Vector2.zero;
+        transform.position = StartingPosition;
+        Rigidbody.isKinematic = false; // Rigidbody를 비키네틱 상태로 설정하여 물리 연산에 반응하게 합니다.
+        enabled = true;
     }
 }
