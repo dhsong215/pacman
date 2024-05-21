a68a0e89257c24a0914d7845f286938dec8ac768
dhsong <dhsong215@gmail.com>
2024-05-21 21:45:42 +0900

    chase 주석 추가

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -8,13 +8,15 @@ public class GhostChase : GhostBehaviour
      }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Node node = other.GetComponent<Node>();
+        Node node = other.GetComponent<Node>(); // 노드와 충돌시 노드 컴포넌트 가져옵니다.
 
+        // frightened모드 우선 수행
         if (node != null && this.enabled && !this.Ghost.Frightened.enabled)
         {
             Vector2 direction = Vector2.zero;
             float minDistance = float.MaxValue;
 
+            // 팩맨에게 가장 가까운 방향을 foreach로 탐색
             foreach (Vector2 availableDirection in node.AvailableDirections)
             {
                 Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
