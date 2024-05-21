06df4b56c1ebeeae614337348da18915c9edcd78
dhsong <dhsong215@gmail.com>
2024-05-21 21:37:29 +0900

    추격모드 스크립트 추가

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -2,4 +2,33 @@ using UnityEngine;
 
 public class GhostChase : GhostBehaviour
 {
+        private void OnDisable()
+    {
+      this.Ghost.Chase.Enable();
+     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && this.enabled && !this.Ghost.Frightened.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float minDistance = float.MaxValue;
+
+            foreach (Vector2 availableDirection in node.AvailableDirections)
+            {
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float distance = (this.Ghost.target.position - newPosition).sqrMagnitude;
+
+                if (distance < minDistance)
+                {
+                    direction = availableDirection;
+                    minDistance = distance;
+                }
+            }
+
+            this.Ghost.Movement.SetDirection(direction);
+
+        }
+    }
 }
