3d6ad36d44b06a55a63b5a1f4473684f4010c859
dhsong <77286910+dhsong215@users.noreply.github.com>
2024-05-29 17:21:48 +0900

    Revert "Feature/player movement"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,79 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 public class GameManager : MonoBehaviour
 {
-    public Ghost[] ghosts;
-    public Pacman pacman;
-    public Transform pellets;
-
-    public int score { get; private set; }
-    public int lives { get; private set; }
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        NewGame();
+        
     }
 
-    private void NewGame()
+    // Update is called once per frame
+    void Update()
     {
-        SetScore(0);
-        SetLives(3);
-        NewRound();
-
+        
     }
 
-    private void NewRound()
+    public void PowerPelletEaten(Pellet pellet)
     {
-        foreach (Transform pellet in this.pellets){
-            pellet.gameObject.SetActive(true);
-        }
-
-        ResetState();
-    }
-
-    private void ResetState()
-    {
-        for (int i=0; i<this.ghosts.Length; i++){
-            this.ghosts[i].gameObject.SetActive(true);
-        }
-
-        this.pacman.gameObject.SetActive(true);
-    }
-
-    private void GameOver()
-    {
-        for (int i=0; i<this.ghosts.Length; i++){
-            this.ghosts[i].gameObject.SetActive(false);
-        }
-
-        this.pacman.gameObject.SetActive(false);
-    }
-
-    private void SetScore(int score)
-    {
-        this.score = score;
-    }
-
-    private void SetLives(int lives)
-    {
-        this.lives = lives;
-    }
-
-    public void GhostEaten(Ghost ghost)
-    {
-        SetScore(this.score + ghost.points);
-    }
-
-    public void PacmanEaten()
-    {
-        this.pacman.gmaeObject.SetActive(false);
-
-        SetLives(this.lives - 1);
-
-        if(this.lives > 0){
-            Invoke(nameof(ResetState), 3.0f);
-        } else {
-            GameOver();
-        }
 
     }
 }
