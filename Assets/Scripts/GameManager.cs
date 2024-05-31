a954484ca6b09725d399efec8db67265a3ca927e
dhsong <dhsong215@gmail.com>
2024-05-31 12:57:24 +0900

    collision with pacman

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
     public Ghost[] ghosts;
     public Pacman pacman;
     public Transform pellets;
